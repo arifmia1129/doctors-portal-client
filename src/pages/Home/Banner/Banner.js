2de9bab82fc5c627ca8b48cd5e3d2331f1ad1926
arifvtti <arif.vtti@gmail.com>
2022-05-10 12:59:15 +0600

    home banner added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,11 +5,11 @@ const Banner = () => {
     return (
         <div className="hero lg:mt-24">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div>
-                    <img src={chair} className="image-full rounded-lg shadow-2xl" />
+                <div className='md:px-10'>
+                    <img src={chair} alt="banner-img" className="image-full rounded-lg shadow-2xl" />
                 </div>
                 <div>
-                    <h1 className="text-5xl font-bold text-accent">Your New Smile Starts Here</h1>
+                    <h1 className="text-4xl font-bold text-accent">Your New Smile Starts Here</h1>
                     <p className="py-6">We want to make you laugh. I want to see your beautiful smile. I want to show your beautiful smile to the world. Let's laugh together.</p>
                     <button className="btn btn-primary text-white font-bold">Get Started</button>
                 </div>
