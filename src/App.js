0107d541a68537903f60e340e0498de5c9813735
arifvtti <arif.vtti@gmail.com>
2022-05-10 11:36:58 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navbar></Navbar>
     </div>
   );
 }
